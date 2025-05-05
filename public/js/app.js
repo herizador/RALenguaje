92bf640b07b2107c07d39ae29af1c11264c77771
herizador <155181668+herizador@users.noreply.github.com>
2025-05-05 21:41:14 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,14 @@
-const API_BASE = '/api/';
+// Reemplaza con tu URL real de Render para BaseX:
+const BASEX_URL = 'https://mi-proyecto-xml-basex.onrender.com';
 
+// Ejemplo: ejecutar XQuery remoto
 async function runXQuery(path) {
-  const res = await fetch(`${API_BASE}basex/xquery/${path}`);
+  const res = await fetch(`${BASEX_URL}/rest/basex/xquery/${path}`);
+  if (!res.ok) throw new Error('Error en XQuery: ' + res.status);
   return await res.text();
 }
+
+// Uso:
+runXQuery('buscar.xq')
+  .then(txt => console.log(txt))
+  .catch(err => console.error(err));
